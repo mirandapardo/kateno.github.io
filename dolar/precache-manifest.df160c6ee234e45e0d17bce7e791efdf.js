1626d188f74ac5cc342996e5a191262f77d7fdcf
Ricardo Miranda <baturen@gmail.com>
2020-06-29 10:07:18 -0400

    dolar

---
+++ dolar/precache-manifest.df160c6ee234e45e0d17bce7e791efdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fb13ca69b1928518f4849eb9375004",
+    "url": "/dolar/index.html"
+  },
+  {
+    "revision": "42c82361c43f09563d18",
+    "url": "/dolar/static/css/main.cde4ced2.chunk.css"
+  },
+  {
+    "revision": "b25d3ce0562926c2ea8a",
+    "url": "/dolar/static/js/2.20fc8492.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/dolar/static/js/2.20fc8492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c82361c43f09563d18",
+    "url": "/dolar/static/js/main.a2fd5e9e.chunk.js"
+  },
+  {
+    "revision": "2ba6c476aeb5e2d79385",
+    "url": "/dolar/static/js/runtime-main.96814b29.js"
+  },
+  {
+    "revision": "48d23102cb2151db62b0d6c944c97057",
+    "url": "/dolar/static/media/logo.48d23102.png"
+  }
+]);
